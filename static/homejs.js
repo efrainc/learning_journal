a79243eca4699d0aa701e4d46a5eb739841a78a6
efrainc <efrainc88@gmail.com>
2015-03-31 14:34:26 -0700

    storing pervious master to checkout a new branch for twitter and ajax

---
+++ static/homejs.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("button#butid").click(function(){
+      add_entry_to_db();
+    });
+});
+
+function add_entry_to_db() {
+      $.post("/add", {'title' : $("[name=title]").val(), 'text' : $("[name=text]").val()},
+        append_entry, "json");
+}
+
+function append_entry(data){
+      var id = data['id'];
+      $('div#newentry').prepend("\
+        <article class='entry' id='entry="+id+"'>\
+        <h3>"+data['title']+"</h3>\
+        <p>dateline</p>\
+        <div class='entry_body'>"+data['text']+"</div>\
+        <a href='/details/"+id+"' id='"+id+"'><button>Detail View</button></a>\
+        <article>\
+        ");
+}
